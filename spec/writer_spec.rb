b7c706821b98938bf039944bc9285ad89741d637
Justin Valentini <justin.valentini@gmail.com>
2014-04-19 18:06:42 -0400

    Add parsing tests to writer to ensure they wrote correctly.

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -1,42 +1,53 @@
 require 'spec_helper'
 require 'axlsx'
+require 'roo'
 
 describe 'Writing' do
   it 'writes a correct file' do
+    filepath = 'templates/correct.xlsx'
+    notebook = "676-1-111"
+
     Axlsx::Package.new do |p|
       spacer = ""
 
       product = "Amichin"
       analyst = "S. Mallon"
       limit = 1
-      notebook = "676-1-111"
 
       p.workbook.add_worksheet(:name => notebook) do |sheet|
         sheet.add_row ["Product", product, spacer, "Analyst", analyst]
         sheet.add_row ["Limit", limit, spacer, "Notebook", notebook]
       end
 
-      p.serialize('templates/correct.xlsx')
+      p.serialize(filepath)
     end
+
+    t = Roo::Spreadsheet.open(filepath)
+    expect(t.sheets.first).to eq(notebook)
   end
 
   it 'writes an incorrect file' do
+    filepath = 'templates/incorrect.xlsx'
+    notebook = "676-1-111"
+    other_notebook = "123-1-111"
+
     Axlsx::Package.new do |p|
       spacer = ""
 
       product = "Amichin"
       analyst = "S. Mallon"
       limit = 1
-      notebook = "676-1-111"
-
-      other_notebook = "123-1-111"
 
       p.workbook.add_worksheet(:name => notebook) do |sheet|
         sheet.add_row ["Product", product, spacer, "Analyst", analyst]
         sheet.add_row ["Limit", limit, spacer, "Notebook", other_notebook]
       end
 
-      p.serialize('templates/incorrect.xlsx')
+      p.serialize(filepath)
     end
+
+    t = Roo::Spreadsheet.open(filepath)
+    expect(t.sheets.first).to eq(notebook)
+    expect(t.cell(2, 5)).to eq(other_notebook)
   end
 end
