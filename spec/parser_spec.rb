f1ae2cbea07dc6809cb2a56a1409c3a23e56b152
Justin Valentini <justin.valentini@gmail.com>
2014-04-21 11:21:32 -0400

    Set parsing test to pending.
    Parser test depends upon a correct.xlsx file which will not exist unless
    the writer tests are run first. That's not a guarantee so set it to
    pending for now.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,6 +3,7 @@ require 'roo'
 
 describe 'Parsing' do
   it 'parses an excel file' do
+    pending
     t = Roo::Spreadsheet.open("templates/correct.xlsx")
     expect(t.sheets.first).to eq("676-1-111")
   end
